409db0185114e7a06cef1f749fb0af9b07fa5e4c
Someone else <random@email.com>
2018-09-24 13:23:35 -0400

    Initiate commit

---
+++ src/com/company/InputAndOutput.java
@@ -0,0 +1,29 @@
+package com.company;
+
+
+//This application takes 2 integer values and finds the largest one. Change the application so that it finds the average of two double values.
+import java.util.Scanner;
+
+public class InputAndOutput {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        double num1, num2, num3;
+
+
+        System.out.print( "First temperature? " );
+        num1 = keyboard.nextDouble();
+
+        System.out.print( "Second temperature? " );
+        num2 = keyboard.nextDouble();
+
+        //System.out.println("The average is : " + Math.max(num1, num2));
+        num3 = ((num1 + num2)/2);
+        System.out.println("The average is : " + (num3));
+
+    }
+}
+
+
